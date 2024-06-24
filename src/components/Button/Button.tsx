ec4815b5a70600dfb9b61fc282517d4d45fb7f0e
devbylanre <devbylanre@gmail.com>
2024-06-24 15:40:57 +0100

    added Button component

---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,146 @@
+import React from 'react';
+import {
+  PolymorphicComponentPropWithRef,
+  PolymorphicRef,
+} from '../../types/polymorphicTypes';
+import { VariantProps } from '../../types/variantType';
+import buttonVariants from './variant';
+
+type ButtonProps = PolymorphicComponentPropWithRef<
+  'button',
+  VariantProps<typeof buttonVariants>
+>;
+
+const Button = React.forwardRef(
+  (props: ButtonProps, ref: PolymorphicRef<'button'>) => {
+    const {
+      as: Component = 'button',
+      className,
+      p,
+      pe,
+      ps,
+      pt,
+      pb,
+      px = 'md',
+      py,
+      m,
+      me,
+      ms,
+      mt,
+      mb,
+      mx,
+      my,
+      gap,
+      gapX,
+      gapY,
+      border = 0,
+      radius = 'lg',
+      borderStyle,
+      borderTop,
+      borderBottom,
+      borderLeft,
+      borderRight,
+      alignSelf,
+      alignContent,
+      alignItems,
+      justifyContent,
+      justifyItems,
+      justifySelf,
+      placeSelf,
+      width,
+      height = 14,
+      scale,
+      scaleX,
+      scaleY,
+      origin,
+      rotate,
+      translateX,
+      translateY,
+      transition = 'all',
+      transitionDelay,
+      transitionDuration = 300,
+      transitionTimingFunction = 'ease-in-out',
+      cursor = 'pointer',
+      pointer,
+      resize,
+      color = 'black',
+      borderColor,
+      backgroundColor = 'primary-60',
+      size = 16,
+      align,
+      weight = 500,
+      transform,
+      verticalAlign,
+      pseudos = { hover: { backgroundColor: 'primary-70' } },
+      ...rest
+    } = props;
+
+    return (
+      <Component
+        ref={ref}
+        className={buttonVariants({
+          p,
+          pe,
+          ps,
+          pt,
+          pb,
+          px,
+          py,
+          m,
+          me,
+          ms,
+          mt,
+          mb,
+          mx,
+          my,
+          gap,
+          gapX,
+          gapY,
+          border,
+          radius,
+          borderStyle,
+          borderTop,
+          borderBottom,
+          borderLeft,
+          borderRight,
+          alignSelf,
+          alignContent,
+          alignItems,
+          justifyContent,
+          justifyItems,
+          justifySelf,
+          placeSelf,
+          width,
+          height,
+          scale,
+          scaleX,
+          scaleY,
+          origin,
+          rotate,
+          translateX,
+          translateY,
+          transition,
+          transitionDelay,
+          transitionDuration,
+          transitionTimingFunction,
+          cursor,
+          pointer,
+          resize,
+          color,
+          borderColor,
+          backgroundColor,
+          size,
+          align,
+          weight,
+          transform,
+          verticalAlign,
+          pseudos,
+          className,
+        })}
+        {...rest}
+      />
+    );
+  }
+);
+
+export default Button;
---
+++ src/components/Button/variant.ts
@@ -0,0 +1,72 @@
+import border from '../../props/border';
+import colors from '../../props/colors';
+import distribution from '../../props/distribution';
+import interaction from '../../props/interaction';
+import sizing from '../../props/sizing';
+import spacing from '../../props/spacing';
+import transform from '../../props/transform';
+import transition from '../../props/transition';
+import typography from '../../props/typography';
+import classVariance from '../../utils/classVariance';
+
+const buttonVariants = classVariance('d-inline-flex', {
+  variants: {
+    p: spacing.p,
+    pe: spacing.pe,
+    ps: spacing.ps,
+    pt: spacing.pt,
+    pb: spacing.pb,
+    px: spacing.px,
+    py: spacing.py,
+    m: spacing.m,
+    me: spacing.me,
+    ms: spacing.ms,
+    mt: spacing.mt,
+    mb: spacing.mb,
+    mx: spacing.mx,
+    my: spacing.my,
+    gap: spacing.gap,
+    gapX: spacing.gapX,
+    gapY: spacing.gapY,
+    border: border.border,
+    radius: border.radius,
+    borderStyle: border.style,
+    borderTop: border.top,
+    borderBottom: border.bottom,
+    borderLeft: border.start,
+    borderRight: border.end,
+    alignItems: distribution.alignItems,
+    alignContent: distribution.alignContent,
+    alignSelf: distribution.alignSelf,
+    justifySelf: distribution.justifySelf,
+    justifyItems: distribution.justifyItems,
+    justifyContent: distribution.justifyContent,
+    placeSelf: distribution.placeSelf,
+    width: sizing.width,
+    height: sizing.height,
+    scale: transform.scale,
+    scaleX: transform.scaleX,
+    scaleY: transform.scaleY,
+    origin: transform.origin,
+    rotate: transform.rotate,
+    translateX: transform.translateX,
+    translateY: transform.translateY,
+    transition: transition.transition,
+    transitionDelay: transition.delay,
+    transitionDuration: transition.duration,
+    transitionTimingFunction: transition.timing,
+    cursor: interaction.cursor,
+    pointer: interaction.pointer,
+    resize: interaction.resize,
+    color: colors.text,
+    borderColor: colors.border,
+    backgroundColor: colors.background,
+    size: typography.size,
+    align: typography.align,
+    weight: typography.weight,
+    transform: typography.transform,
+    verticalAlign: typography.verticalAlign,
+  },
+});
+
+export default buttonVariants;
