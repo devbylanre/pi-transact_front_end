1829b57563bfd199eda2be6b34f3ffbbaf0ae656
devbylanre <devbylanre@gmail.com>
2024-06-24 12:41:51 +0100

    added Container component

---
+++ src/components/Container/Container.tsx
@@ -0,0 +1,134 @@
+import React from 'react';
+import {
+  PolymorphicComponentPropWithRef,
+  PolymorphicRef,
+} from '../../types/polymorphicTypes';
+import { VariantProps } from '../../types/variantType';
+import containerVariants from './variants';
+
+type ContainerElements = 'div' | 'span';
+type ContainerProps = PolymorphicComponentPropWithRef<
+  ContainerElements,
+  VariantProps<typeof containerVariants>
+>;
+
+const Container = React.forwardRef(
+  (props: ContainerProps, ref: PolymorphicRef<ContainerElements>) => {
+    const {
+      as,
+      className,
+      m,
+      mb,
+      mt,
+      ms,
+      me,
+      mx,
+      my,
+      p,
+      pt,
+      pb,
+      ps,
+      pe,
+      px,
+      py,
+      spaceX,
+      spaceY,
+      top,
+      left,
+      bottom,
+      right,
+      display,
+      position,
+      container,
+      border,
+      radius,
+      borderStyle,
+      borderBottom,
+      borderTop,
+      borderLeft,
+      borderRight,
+      width,
+      height,
+      scale,
+      scaleX,
+      scaleY,
+      origin,
+      rotate,
+      translateX,
+      translateY,
+      transition,
+      transitionDuration,
+      transitionDelay,
+      transitionTimingFunction,
+      cursor,
+      pointer,
+      resize,
+      borderColor,
+      backgroundColor,
+      pseudos,
+      ...rest
+    } = props;
+    const Component = (as || 'div') as React.ElementType;
+
+    return (
+      <Component
+        ref={ref}
+        className={containerVariants({
+          m,
+          mb,
+          mt,
+          ms,
+          me,
+          mx,
+          my,
+          p,
+          pt,
+          pb,
+          ps,
+          pe,
+          px,
+          py,
+          spaceX,
+          spaceY,
+          top,
+          left,
+          bottom,
+          right,
+          display,
+          position,
+          container,
+          border,
+          radius,
+          borderStyle,
+          borderBottom,
+          borderTop,
+          borderLeft,
+          borderRight,
+          width,
+          height,
+          scale,
+          scaleX,
+          scaleY,
+          origin,
+          rotate,
+          translateX,
+          translateY,
+          transition,
+          transitionDuration,
+          transitionDelay,
+          transitionTimingFunction,
+          cursor,
+          pointer,
+          resize,
+          borderColor,
+          backgroundColor,
+          pseudos,
+          className,
+        })}
+        {...rest}
+      />
+    );
+  }
+);
+
+export default Container;
---
+++ src/components/Container/variants.ts
@@ -0,0 +1,64 @@
+import border from '../../props/border';
+import colors from '../../props/colors';
+import interaction from '../../props/interaction';
+import layout from '../../props/layout';
+import sizing from '../../props/sizing';
+import spacing from '../../props/spacing';
+import transform from '../../props/transform';
+import transition from '../../props/transition';
+import classVariance from '../../utils/classVariance';
+
+const containerVariants = classVariance('w-max', {
+  variants: {
+    m: spacing.m,
+    mb: spacing.mb,
+    mt: spacing.mt,
+    ms: spacing.ms,
+    me: spacing.me,
+    mx: spacing.mx,
+    my: spacing.my,
+    p: spacing.p,
+    pt: spacing.pt,
+    pb: spacing.pb,
+    ps: spacing.ps,
+    pe: spacing.pe,
+    px: spacing.px,
+    py: spacing.py,
+    spaceX: spacing.spaceX,
+    spaceY: spacing.spaceY,
+    top: layout.top,
+    left: layout.left,
+    bottom: layout.bottom,
+    right: layout.right,
+    display: layout.display,
+    position: layout.position,
+    container: layout.container,
+    border: border.border,
+    radius: border.radius,
+    borderStyle: border.style,
+    borderBottom: border.bottom,
+    borderTop: border.top,
+    borderLeft: border.start,
+    borderRight: border.end,
+    width: sizing.width,
+    height: sizing.height,
+    scale: transform.scale,
+    scaleX: transform.scaleX,
+    scaleY: transform.scaleY,
+    origin: transform.origin,
+    rotate: transform.rotate,
+    translateX: transform.translateX,
+    translateY: transform.translateY,
+    transition: transition.transition,
+    transitionDuration: transition.duration,
+    transitionDelay: transition.delay,
+    transitionTimingFunction: transition.timing,
+    cursor: interaction.cursor,
+    pointer: interaction.pointer,
+    resize: interaction.resize,
+    borderColor: colors.border,
+    backgroundColor: colors.background,
+  },
+});
+
+export default containerVariants;
