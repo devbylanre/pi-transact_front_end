d5bc2fea76d3f03f62497b2a55efbe626fa7b003
devbylanre <devbylanre@gmail.com>
2024-06-24 12:42:18 +0100

    added Flex component

---
+++ src/components/Flex/variants.ts
@@ -0,0 +1,83 @@
+import border from '../../props/border';
+import colors from '../../props/colors';
+import distribution from '../../props/distribution';
+import flex from '../../props/flex';
+import grid from '../../props/grid';
+import interaction from '../../props/interaction';
+import layout from '../../props/layout';
+import sizing from '../../props/sizing';
+import spacing from '../../props/spacing';
+import transform from '../../props/transform';
+import transition from '../../props/transition';
+import classVariance from '../../utils/classVariance';
+
+const flexVariants = classVariance('', {
+  variants: {
+    m: spacing.m,
+    mb: spacing.mb,
+    mt: spacing.mt,
+    ms: spacing.ms,
+    me: spacing.me,
+    mx: spacing.mx,
+    my: spacing.my,
+    p: spacing.p,
+    pt: spacing.pt,
+    pb: spacing.pb,
+    ps: spacing.ps,
+    pe: spacing.pe,
+    px: spacing.px,
+    py: spacing.py,
+    spaceX: spacing.spaceX,
+    spaceY: spacing.spaceY,
+    top: layout.top,
+    left: layout.left,
+    bottom: layout.bottom,
+    right: layout.right,
+    display: layout.display,
+    position: layout.position,
+    border: border.border,
+    radius: border.radius,
+    borderStyle: border.style,
+    borderBottom: border.bottom,
+    borderTop: border.top,
+    borderLeft: border.start,
+    borderRight: border.end,
+    width: sizing.width,
+    height: sizing.height,
+    scale: transform.scale,
+    scaleX: transform.scaleX,
+    scaleY: transform.scaleY,
+    origin: transform.origin,
+    rotate: transform.rotate,
+    translateX: transform.translateX,
+    translateY: transform.translateY,
+    transition: transition.transition,
+    transitionDuration: transition.duration,
+    transitionDelay: transition.delay,
+    transitionTimingFunction: transition.timing,
+    cursor: interaction.cursor,
+    pointer: interaction.pointer,
+    resize: interaction.resize,
+    borderColor: colors.border,
+    backgroundColor: colors.background,
+    flex: flex.flex,
+    flexBasis: flex.basis,
+    flexWrap: flex.wrap,
+    flexDirection: flex.direction,
+    alignSelf: distribution.alignSelf,
+    placeSelf: distribution.placeSelf,
+    justifySelf: distribution.justifySelf,
+    alignItems: distribution.alignItems,
+    alignContent: distribution.alignContent,
+    justifyItems: distribution.justifyItems,
+    justifyContent: distribution.justifyContent,
+    gridRowSpan: grid.gridRowSpan,
+    gridColumnSpan: grid.gridColumnSpan,
+    gridRowStart: grid.gridRowStart,
+    gridRowEnd: grid.gridRowEnd,
+    gridColumnStart: grid.gridColumnStart,
+    gridColumnEnd: grid.gridColumnEnd,
+  },
+});
+
+export default flexVariants;
