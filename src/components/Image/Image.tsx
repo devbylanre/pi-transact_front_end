31fb277f932b3a1d01499a600a2b8eb2e00de8b4
devbylanre <devbylanre@gmail.com>
2024-06-26 10:39:56 +0100

    added Image component

---
+++ src/components/Image/Image.tsx
@@ -0,0 +1,113 @@
+import React from 'react';
+import {
+  PolymorphicComponentPropWithRef,
+  PolymorphicRef,
+} from '../../types/polymorphicTypes';
+import { VariantProps } from '../../types/variantType';
+import imageVariants from './variants';
+
+export type ImageProps = PolymorphicComponentPropWithRef<
+  'img',
+  VariantProps<typeof imageVariants>
+>;
+
+const Image = React.forwardRef(
+  (props: ImageProps, ref: PolymorphicRef<'img'>) => {
+    const {
+      as = 'img',
+      className,
+      p,
+      px,
+      py,
+      pe,
+      ps,
+      pt,
+      pb,
+      m,
+      mx,
+      my,
+      ms,
+      me,
+      mt,
+      mb,
+      objectFit,
+      radius,
+      border,
+      borderTop,
+      borderBottom,
+      borderLeft,
+      borderRight,
+      borderStyle,
+      backgroundCOlor,
+      borderColor,
+      width,
+      height,
+      size,
+      scale,
+      scaleX,
+      scaleY,
+      rotate,
+      origin,
+      translateX,
+      translateY,
+      transition,
+      transitionDelay,
+      transitionDuration,
+      transitionTimingFunction,
+      pseudos,
+      ...rest
+    } = props;
+    const Component = as || 'img';
+
+    return (
+      <Component
+        ref={ref}
+        className={imageVariants({
+          p,
+          px,
+          py,
+          pe,
+          ps,
+          pt,
+          pb,
+          m,
+          mx,
+          my,
+          ms,
+          me,
+          mt,
+          mb,
+          objectFit,
+          radius,
+          border,
+          borderTop,
+          borderBottom,
+          borderLeft,
+          borderRight,
+          borderStyle,
+          backgroundCOlor,
+          borderColor,
+          width,
+          height,
+          size,
+          scale,
+          scaleX,
+          scaleY,
+          rotate,
+          origin,
+          translateX,
+          translateY,
+          transition,
+          transitionDelay,
+          transitionDuration,
+          transitionTimingFunction,
+          pseudos,
+          className,
+        })}
+        {...rest}
+      />
+    );
+  }
+);
+
+export default Image;
---
+++ src/components/Image/variants.tsx
@@ -0,0 +1,53 @@
+import border from '../../props/border';
+import colors from '../../props/colors';
+import layout from '../../props/layout';
+import sizing from '../../props/sizing';
+import spacing from '../../props/spacing';
+import transform from '../../props/transform';
+import transition from '../../props/transition';
+import classVariance from '../../utils/classVariance';
+
+const imageVariants = classVariance('', {
+  variants: {
+    p: spacing.p,
+    px: spacing.px,
+    py: spacing.py,
+    pe: spacing.pe,
+    ps: spacing.ps,
+    pt: spacing.pt,
+    pb: spacing.pb,
+    m: spacing.m,
+    mx: spacing.mx,
+    my: spacing.my,
+    ms: spacing.ms,
+    me: spacing.me,
+    mt: spacing.mt,
+    mb: spacing.mb,
+    objectFit: layout.objectFit,
+    radius: border.radius,
+    border: border.border,
+    borderTop: border.top,
+    borderBottom: border.bottom,
+    borderLeft: border.start,
+    borderRight: border.end,
+    borderStyle: border.style,
+    backgroundCOlor: colors.background,
+    borderColor: colors.border,
+    width: sizing.width,
+    height: sizing.height,
+    size: sizing.size,
+    scale: transform.scale,
+    scaleX: transform.scaleX,
+    scaleY: transform.scaleY,
+    rotate: transform.rotate,
+    origin: transform.origin,
+    translateX: transform.translateX,
+    translateY: transform.translateY,
+    transition: transition.transition,
+    transitionDelay: transition.delay,
+    transitionDuration: transition.duration,
+    transitionTimingFunction: transition.timing,
+  },
+});
+
+export default imageVariants;
