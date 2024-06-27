8105c1291e94d5abbed58363a46c8f3a968cd465
devbylanre <devbylanre@gmail.com>
2024-06-27 15:18:28 +0100

    added HelmetProvider component

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,15 +1,22 @@
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import IndexPage from './pages/IndexPage';
+import RootLayout from './layouts/root/Layout';
+import { HelmetProvider } from 'react-helmet-async';
 
 const router = createBrowserRouter([
   {
     path: '/',
+    element: <RootLayout />,
     children: [{ index: true, element: <IndexPage /> }],
   },
 ]);
 
 const Router = () => {
-  return <RouterProvider router={router} />;
+  return (
+    <HelmetProvider>
+      <RouterProvider router={router} />
+    </HelmetProvider>
+  );
 };
 
 export default Router;
