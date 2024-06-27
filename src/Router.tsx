a7db308289ab874c7eb7022fe71d34fd6a49d80f
devbylanre <devbylanre@gmail.com>
2024-06-27 15:26:20 +0100

    added AboutPage component

--- src/Router.tsx
+++ src/Router.tsx
@@ -2,12 +2,16 @@ import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import IndexPage from './pages/IndexPage';
 import RootLayout from './layouts/root/Layout';
 import { HelmetProvider } from 'react-helmet-async';
+import AboutPage from './pages/AboutPage';
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <RootLayout />,
-    children: [{ index: true, element: <IndexPage /> }],
+    children: [
+      { index: true, element: <IndexPage /> },
+      { path: 'about', element: <AboutPage /> },
+    ],
   },
 ]);
 
