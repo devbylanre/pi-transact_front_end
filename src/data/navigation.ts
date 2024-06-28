16032937dff8db9b46161725a22a2646741ff1b3
devbylanre <devbylanre@gmail.com>
2024-06-28 17:12:25 +0100

    added navigation data

---
+++ src/data/navigation.ts
@@ -0,0 +1,60 @@
+const navigation = {
+  products: {
+    categories: [
+      {
+        name: 'Users',
+        subCategories: [
+          { name: 'Authentication', path: '' },
+          { name: 'Access management', path: '' },
+          { name: 'Cookie policy', path: '' },
+        ],
+      },
+      {
+        name: 'Updates',
+        subCategories: [
+          { name: 'Features drop', path: '' },
+          { name: 'Upcoming', path: '' },
+        ],
+      },
+      {
+        name: 'Monetization',
+        subCategories: [
+          { name: 'Manage business', path: '' },
+          { name: 'Invoices', path: '' },
+        ],
+      },
+      {
+        name: 'B2B',
+        subCategories: [{ name: 'Billing', path: '' }],
+      },
+    ],
+  },
+  resources: {
+    categories: [
+      {
+        name: 'Learn',
+        subCategories: [
+          { name: 'Watch a demo', path: '' },
+          { name: 'Documentation', path: '' },
+          { name: 'Blog', path: '' },
+          { name: 'Roadmap', path: '' },
+          { name: 'Security', path: '' },
+          { name: 'About Pi-Transact', path: '' },
+        ],
+      },
+      {
+        name: 'Get help',
+        subCategories: [
+          { name: 'Twitter handle', path: '' },
+          { name: 'Support', path: '' },
+          { name: 'Chat with an agent', path: '' },
+          { name: 'System status', path: '' },
+        ],
+      },
+    ],
+  },
+  customers: '',
+  pricing: '',
+};
+
+export default navigation;
