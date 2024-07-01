54f853a398b10296337688d55570d9172b6359b1
devbylanre <devbylanre@gmail.com>
2024-07-01 13:45:56 +0100

    layout improvement

--- src/layouts/auth/Layout.tsx
+++ src/layouts/auth/Layout.tsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import Section from '../../components/Section/Section';
 import Container from '../../components/Container/Container';
-import { Outlet } from 'react-router-dom';
+import { Link, Outlet } from 'react-router-dom';
 import Flex from '../../components/Flex/Flex';
 import Text from '../../components/Text/Text';
 import Box from '../../components/Box/Box';
+import Footer from './components/Footer';
 
 const AuthLayout = () => {
   return (
-    <Section>
+    <Section px={'lg'}>
       <Container
         mx={'auto'}
         container={'xxs'}
@@ -18,15 +19,22 @@ const AuthLayout = () => {
           style={{ minHeight: '100vh' }}
           py={{ initial: '2xl', sm: '8xl' }}
         >
-          <Text
-            weight={500}
-            size={{ initial: 21, sm: 25 }}
+          <Link
+            to={'/'}
+            style={{ textDecoration: 'none' }}
           >
-            Pi-transact
-          </Text>
+            <Text
+              weight={500}
+              size={{ initial: 21, sm: 25 }}
+            >
+              Pi-transact
+            </Text>
+          </Link>
           <Box mt={{ initial: '2xl', sm: '4xl' }}>
             <Outlet />
           </Box>
+
+          <Footer />
         </Flex>
       </Container>
     </Section>
---
+++ src/layouts/auth/components/Footer.tsx
@@ -0,0 +1,57 @@
+import React from 'react';
+import Box from '../../../components/Box/Box';
+import Text from '../../../components/Text/Text';
+import { Link, useLocation } from 'react-router-dom';
+import Flex from '../../../components/Flex/Flex';
+import navigation from '../../../data/navigation';
+
+const Footer = () => {
+  const { pathname } = useLocation();
+  const PATH = pathname.split('/')[2];
+
+  console.log({ PATH });
+
+  return (
+    <Box
+      pb={'xl'}
+      mt={'5xl'}
+      spaceY={'4xl'}
+    >
+      <Text
+        as={'p'}
+        size={16}
+        align={'center'}
+      >
+        {PATH === 'new' ? 'Already have an account? ' : 'No account? '}
+        <Text size={'inherit'}>
+          <Link to={PATH === 'new' ? navigation.login : navigation.register}>
+            {PATH === 'new' ? 'Sign in' : 'Sign up'}
+          </Link>
+        </Text>
+      </Text>
+
+      <Box spaceY={'xs'}>
+        <Text
+          as={'p'}
+          size={16}
+          align={'center'}
+        >
+          By continuing, you agree to our policies
+        </Text>
+        <Flex
+          justifyContent={'center'}
+          gapX={'md'}
+        >
+          <Text size={16}>
+            <Link to={navigation.login}>Terms of use</Link>
+          </Text>
+          <Text size={16}>
+            <Link to={navigation.login}>Privacy policy</Link>
+          </Text>
+        </Flex>
+      </Box>
+    </Box>
+  );
+};
+
+export default Footer;
